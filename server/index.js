d2a21f2a0766ad8b36bb604f757ee83e3919f555
Fedor <email@gmail.com>
2024-08-04 18:19:19 +0300

    start

---
+++ server/index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const port = process.env.PORT; 
+
+async function startServer() {
+  try {
+    await require("./database").setupDB();
+
+    const app = require("./api");
+
+    const httpServer = require("http").createServer(app);
+
+    const io = require("./api/socketServer").setupSocketServer(httpServer);
+
+    app.set("io", io);
+
+    httpServer.listen(port, () => {
+      console.log(`App listening on port ${port}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+startServer();
